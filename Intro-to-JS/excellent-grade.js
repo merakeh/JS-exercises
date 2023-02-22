f944397bac1c47bad4e4e29812d2924fce1984b4
Mediha Kehayova <99451787+merakeh@users.noreply.github.com>
2023-02-22 23:05:54 +0200

    Some exercise on JS syntax

---
+++ Intro-to-JS/excellent-grade.js
@@ -0,0 +1,10 @@
+function solve(grade){
+    if (grade >= 5.50 && grade<= 6){
+        console.log("Excellent");
+    } else {
+        console.log("Not excellent");
+    }
+}
+
+solve(6.59);
+solve(5.59);
